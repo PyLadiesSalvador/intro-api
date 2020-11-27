c794f96491125e791e40566310b1f919d7845bbc
lorenaps <lorena.santpe@gmail.com>
2020-11-27 00:13:45 -0300

    Adicionando get top candidatos votados

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ que são uma espécie de interface com os dados ou rotinas de outro sistema.
 
 O Flask é um framework que facilita a construção de APIs em Python,
 em sua essência procura manter uma estrutura simples mas extensível, 
-permitindo várias customizações a partir de outras bibliotecas.
+permitindo várias customizações e funcionalidades a partir de outras bibliotecas.
 
 ## Comandos:
 
--- run.py
+++ run.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
+
 
 app = Flask('app')
+
 
 @app.route("/")
 def hello():
     return "Hello World in Flask!"
 
+
 @app.route("/<name>")
 def index(name):
-
     return "Olá {}".format(name), 200
+
 
 @app.route("/html/<nome>")
 def html_page(nome):
-
     return render_template("html_page.html", nome=nome)
+
 
 @app.route("/jsonify")
 def json_api_jsonify():
@@ -27,11 +30,26 @@ def json_api_jsonify():
 
     return jsonify(pessoas=pessoas, total=len(pessoas))
 
-@app.route("/eleicao2020/")
-def get_eleitos():
-    candidatas = []
 
-    return jsonify(eleitas=candidatas, total=len(candidatas))
+@app.route("/eleicao2020/")
+def get_top_votos_candidatos():
+    cargo = request.args.get('cargo', 'Vereador')
+    indice = request.args.get('indice', 3)
+
+    candidatos = get_top_votos(cargo, indice)
+
+    return jsonify(candidatos=candidatos)
+
+
+def get_top_votos(cargo, indice):
+    import pandas as pd
+
+    dados = pd.readcsv('dados/dados.csv')
+    dados = dados.loc[dados['CARGO'] == cargo]
+    dados = dados.sort_values('QTD_VOTOS', ascending=False).head(indice)
+
+    return dados
+
 
 app.run(debug=True, use_reloader=True)
 

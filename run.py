25c7a704dfd45ebbe6ee0ed8d78cd5a17daf7417
Lorena Pereira <lorena.santpe@gmail.com>
2022-11-05 00:22:44 -0300

    Atualizando exemplos de introdução
    Atualizando exemplos de introdução

--- run.py
+++ run.py
@@ -10,7 +10,6 @@ app = Flask('app')
 def hello():
     return "Hello World in Flask!"
 
-
 @app.route("/<name>")
 def index(name):
     return "Olá {}".format(name), 200
@@ -20,36 +19,45 @@ def index(name):
 def html_page(nome):
     return render_template("html_page.html", nome=nome)
 
+@app.route("/html/dog")
+def html_dog():
+    import requests
+
+    nome = request.args.get('nome')
+
+    # API utilizada é a TheDogApi - https://docs.thedogapi.com/
+    dogImage = requests.get('https://api.thedogapi.com/v1/images/search')
+    url = dogImage.json()[0].get('url')
+    
+    return render_template("html_page.html", nome=nome, image=url)
 
 @app.route("/jsonify")
 def json_api_jsonify():
-    pessoas = [{"nome": "Ana", "idade": 25},
+    pessoas = [{"nome": "Ana", "idade": 35},
                {"nome": "Claudia", "idade": 26},
-               {"nome": "Priscila", "idade": 27},
-               {"nome": "Carine", "idade": 28}]
+               {"nome": "Priscila", "idade": 42},
+               {"nome": "Carine", "idade": 22}]
 
     return jsonify(pessoas=pessoas, total=len(pessoas))
 
-
-@app.route("/eleicao2020/")
+@app.route("/topvereadores2020/")
 def get_top_votos_candidatos():
     cargo = request.args.get('cargo', 'Vereador')
     indice = request.args.get('indice', 3)
 
-    candidatos = get_top_votos(cargo, indice)
+    votacoes = get_top_votos(cargo, indice)
 
-    return jsonify(candidatos=candidatos)
-
+    return jsonify(votacoes.to_dict(orient ='records'))
 
 def get_top_votos(cargo, indice):
     import pandas as pd
 
-    dados = pd.readcsv('dados/dados.csv')
-    dados = dados.loc[dados['CARGO'] == cargo]
-    dados = dados.sort_values('QTD_VOTOS', ascending=False).head(indice)
+    dados = pd.read_csv('dados/dados.csv', usecols ='''NM_URNA_CANDIDATO SG_PARTIDO 
+        TOTAL_VOTOS NM_MUNICIPIO DS_CARGO'''.split(), sep=",")
+    dados = dados.loc[dados['DS_CARGO'] == cargo][['NM_URNA_CANDIDATO', 'SG_PARTIDO', 'TOTAL_VOTOS']]
+    dados = dados.sort_values('TOTAL_VOTOS', ascending=False).head(indice)
 
     return dados
-
 
 app.run(debug=True, use_reloader=True)
 
